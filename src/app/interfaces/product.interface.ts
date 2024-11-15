8cd38212f0f05ad9adb35a9ba7237615503f58cb
diego.galvan <diego.galvan@teit.cfe.mx>
2024-11-15 15:48:51 -0600

    update: interface Product

--- src/app/interfaces/product.interface.ts
+++ src/app/interfaces/product.interface.ts
@@ -1,8 +1,10 @@
 
 export interface Product {
+    id            : number;
     name          : string;
     subInformation: string;
     imgUrl        : string;
     price         : number;
     stock         : number;
+    amount        : number;
 }
